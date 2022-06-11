747f920dc2c226b1ba368dc09267a800c75797d6
Irelandoracle1 <nairajnr1@gmail.com>
2022-06-11 23:42:51 +0000

    created the class constructor which is the init method

--- run.py
+++ run.py
@@ -2,3 +2,10 @@ class MyBattleship:
     """
     This class holds all the code for the battleship game
     """
+    def __init__(self):
+        """
+        This is the class constructor method for
+        initializing the game board and also
+        for calling defined class methods
+        """
+        
