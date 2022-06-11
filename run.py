86845ba1ac09a79bf5a4c6a16a4e7ee08be76ca4
Irelandoracle1 <nairajnr1@gmail.com>
2022-06-11 23:49:01 +0000

    created a board attribute and initialized it with an empty list and also populated the empty board list with values inside the class constructor

--- run.py
+++ run.py
@@ -2,10 +2,15 @@ class MyBattleship:
     """
     This class holds all the code for the battleship game
     """
+    #define a board attribute with an empty list
+    board=[]
     def __init__(self):
         """
         This is the class constructor method for
         initializing the game board and also
         for calling defined class methods
         """
-        
+        for b in range(10):
+            self.board.append(["0"]*10)
+
+battleship_game=MyBattleship()
