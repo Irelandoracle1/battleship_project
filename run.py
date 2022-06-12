2154c5af788813dfbab66308884efdae18aada4d
Irelandoracle1 <nairajnr1@gmail.com>
2022-06-12 00:02:43 +0000

    looped through the board list to display the game board

--- run.py
+++ run.py
@@ -19,7 +19,10 @@ class MyBattleship:
         This method displays the game board
         with the values in the board list
         """
+        v=0
+        print("  0 1 2 3 4 5 6 7 8 9 ")
         for row in self.board:
-            print(" ".join(row))
+            print(v, " ".join(row))
+            v+=1
 
 battleship_game=MyBattleship()
