ccf7bdd516af9cffd53d28285fe683e2682d1b8f
Irelandoracle1 <nairajnr1@gmail.com>
2022-06-12 00:36:22 +0000

    added a logic for user to select grid or coordinates for hitting the ship and also added a check for a user choosing a value that is out of range

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ class MyBattleship:
         for b in range(10):
             self.board.append(["0"]*10)
         self.display_board()
+        self.play_game()
 
     def display_board(self):
         """
@@ -26,11 +27,35 @@ class MyBattleship:
             print(v, " ".join(row))
             v+=1
     def random_row(self):
+        """
+        Generate random value for the board row
+        """
         return random.randint(0, 10-1)
+
     def random_column(self):
+        """
+        Generate random value for the game column
+        """
         return random.randint(0, 10-1)
+
+    def play_game(self):
+        """
+        This method contains the logic for the user to play
+        the battleship game
+        """
+        for play in range(10):
+            guess_row=int(input("Guess_Row:"))
+            guess_column=int(input("Guess_Column:"))
+            if guess_row==self.random_row() and guess_column==random_column:
+                print("Congrats! You hit the ship.\n")
+                print("You Won!")
+                break;
+            else:
+                if (guess_row<0 or guess_row>9) or (guess_column<0 or guess_column>9):
+                    print("Sorry your guess was out of range")
+
+    
 
 
 battleship_game=MyBattleship()
-rand_row=battleship_game.random_row()
-print(rand_row)
+
